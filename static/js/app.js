fa26918774928f9acdf1d5e8973ba981d6fa1e27
Sayan Goswami <goswami.sayan47@gmail.com>
2017-12-20 12:28:00 +0530

    fix SSL issues

--- static/js/app.js
+++ static/js/app.js
@@ -16,7 +16,7 @@ var COLORS = [
 ];
 
 // socket
-var socket = io.connect('http://' + document.domain + ':' + location.port + "/v1");
+var socket = io.connect(location.protocol + location.hostname + ':' + location.port + "/v1");
 
 // new connection
 socket.on('connect', function() {
